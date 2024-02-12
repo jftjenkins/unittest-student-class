04d360336802d0d5711e4452638e0945e7412827
jftjenkins <jft.jenkins@gmail.com>
2024-02-12 13:23:47 +0000

    Tested and implemented apply_extension method.

--- student.py
+++ student.py
@@ -18,7 +18,11 @@ class Student:
     def email(self):
         return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
 
+
     def alert_santa(self):
         self.naughty_list = True
 
+    
+    def apply_extension(self, days):
+        self.end_date = self.end_date + timedelta(days=days)
 
